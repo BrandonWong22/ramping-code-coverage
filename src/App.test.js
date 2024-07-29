fb5aa5b36395e2cc97a9b88d9d12b7327dad775c
brandon <bwong@konradgroup.com>
2024-07-29 15:48:20 -0400

    update

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import App from './App';
 import { render, screen } from '@testing-library/react';
 
-test('Always true test', () => {
+test.only('Always true test', () => {
   expect(true).toBe.true;
 });
 
