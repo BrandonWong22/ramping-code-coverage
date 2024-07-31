f678ce6a0608cf08d403f1b87a9ebe164ad6c74d
brandon <bwong@konradgroup.com>
2024-07-31 13:26:33 -0400

    update tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,8 @@
 import App from './App';
 import { render, screen } from '@testing-library/react';
+import Test from './components/Test';
 
-test('Always true test', () => {
+test.only('Always true test', () => {
   expect(true).toBe.true;
 });
 
@@ -9,6 +10,14 @@ test('Heading should be Vite + React', () => {
   render(<App />);
 
   const headingElement = screen.getByText('Vite + React');
+
+  expect(headingElement).toBeInTheDocument();
+});
+
+test('Heading should be Vite + React', () => {
+  render(<Test />);
+
+  const headingElement = screen.getByText('Hello');
 
   expect(headingElement).toBeInTheDocument();
 });
