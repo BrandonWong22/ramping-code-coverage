e4e28a7c5efe906867507702a90a39dc976b4775
brandon <bwong@konradgroup.com>
2024-08-01 15:09:16 -0400

    update test files

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-import Test from './components/Test';
+import Test from './components/Header';
 
 function App() {
   const [count, setCount] = useState(0);
--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,12 @@
 import App from './App';
 import { render, screen } from '@testing-library/react';
-import Test from './components/Test';
+import Test from './components/Header';
 
 test('Always true test', () => {
   expect(true).toBe.true;
 });
 
-test.skip('Heading should be Vite + React', () => {
+test('Heading should be Vite + React', () => {
   render(<App />);
 
   const headingElement = screen.getByText('Vite + React');
@@ -14,10 +14,10 @@ test.skip('Heading should be Vite + React', () => {
   expect(headingElement).toBeInTheDocument();
 });
 
-test('Heading should be Vite + React', () => {
+test('Header component renders the string Header', () => {
   render(<Test />);
 
-  const headingElement = screen.getByText('Hello');
+  const headingElement = screen.getByText('Header');
 
   expect(headingElement).toBeInTheDocument();
 });
