0e482a126bdb8a5d569f203ca8669a18a280d578
brandon <bwong@konradgroup.com>
2024-08-01 10:34:44 -0400

    update script

--- src/App.test.js
+++ src/App.test.js
@@ -2,11 +2,11 @@ import App from './App';
 import { render, screen } from '@testing-library/react';
 import Test from './components/Test';
 
-test('Always true test', () => {
+test.only('Always true test', () => {
   expect(true).toBe.true;
 });
 
-test.skip('Heading should be Vite + React', () => {
+test('Heading should be Vite + React', () => {
   render(<App />);
 
   const headingElement = screen.getByText('Vite + React');
