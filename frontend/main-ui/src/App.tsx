07213ac2d002d98e68818110648ed65a753bd04c
brandon <bwong@konradgroup.com>
2024-08-08 12:25:47 -0400

    make frontend ts

--- frontend/main-ui/src/App.tsx
+++ frontend/main-ui/src/App.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-import InsecureComponent from './components/InsecureComponent';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -18,14 +17,12 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h2>Main UI!!</h2>
-      <InsecureComponent />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR!
+          Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
--- frontend/main-ui/src/main-ui.test.tsx
+++ frontend/main-ui/src/main-ui.test.tsx
@@ -1,19 +1,19 @@
 import App from './App';
 import { render, screen } from '@testing-library/react';
-import Header from './components/Header';
+import HeadingComponent from './HeadingComponent';
+
+test('Always true test', () => {
+  expect(true).toBeTruthy;
+});
 
 test('Heading should be Vite + React', () => {
   render(<App />);
-
   const headingElement = screen.getByText('Vite + React');
-
   expect(headingElement).toBeInTheDocument();
 });
 
-test('Header component renders the string Header', () => {
-  render(<Header />);
-
-  const headingElement = screen.getByText('Header');
-
+test('Heading component', () => {
+  render(<HeadingComponent />);
+  const headingElement = screen.getByText('Hello');
   expect(headingElement).toBeInTheDocument();
 });
