c6fddffae88828216c2f501bab3fd44400dbce4f
Danny Menendez <dmenendez@konradgroup.com>
2024-08-08 15:51:20 -0600

    added failing maven dependency

--- backend/main-backend/src/test/java/com/backend_team/main_backend/services/TestMerchantService.java
+++ backend/main-backend/src/test/java/com/backend_team/main_backend/services/TestMerchantService.java
@@ -19,9 +19,9 @@ public class TestMerchantService {
         assertEquals(5, merchantService.calculateShippingFee(1));
     }
 
-    @Test
-    public void failingTest() {
-        MerchantService merchantService = new MerchantService();
-        assertEquals(15, merchantService.calculateShippingFee(1));
-    }
+//    @Test
+//    public void failingTest() {
+//        MerchantService merchantService = new MerchantService();
+//        assertEquals(15, merchantService.calculateShippingFee(1));
+//    }
 }
