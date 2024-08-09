48f2619291c2ce881b2d3c738076813adb8789fe
brandon <bwong@konradgroup.com>
2024-08-09 11:39:17 -0400

    fix tests

--- frontend/main-ui/src/main-ui.test.tsx
+++ frontend/main-ui/src/main-ui.test.tsx
@@ -2,13 +2,13 @@ import App from './App';
 import { render, screen } from '@testing-library/react';
 import HeadingComponent from './HeadingComponent';
 
-test.skip('Heading should be Vite + React', () => {
+test('Heading should be Vite + React', () => {
   render(<App />);
   const headingElement = screen.getByText('Vite + React');
   expect(headingElement).toBeInTheDocument();
 });
 
-test.skip('Heading component', () => {
+test('Heading component', () => {
   render(<HeadingComponent />);
   const headingElement = screen.getByText('Hello');
   expect(headingElement).toBeInTheDocument();
